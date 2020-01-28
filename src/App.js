4f8c0fb1680bf19a4252ea06c008fc39511301e7
SpzGen <zvezdochkin@mail.ru>
2020-01-28 21:16:49 +0300

    sddsgdfbsdbf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Атом 1111
+          Атом 111122132sfzvsd
         </p>
         <a
           className="App-link"
